1e8e14c8040ff7d17ece1e60417c15326f117527
Thaynara C Rodrigues <60945730+ThaynaraCR@users.noreply.github.com>
2020-07-18 14:29:49 -0300

    Arquivos principais

---
+++ routes.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const routes = express.Router()
+
+
+let db = [
+    { '1': { Nome: 'Cliente1', Idade: '20' } },
+    { '2': { Nome: 'Cliente2', Idade: '26' } },
+    { '3': { Nome: 'Cliente3', Idade: '25' } }
+]
+
+routes.get('/', (req, res) => {
+    return res.json(db)
+})
+
+
+routes.post('/add', (req, res) => {
+    const body = req.body
+
+    if (!body)
+        return res.status(400).end()
+
+    db.push(body)
+    return res.json(body)
+})
+
+routes.delete('/:id', (req, res) =>{
+    const id = req.params.id
+
+    let newDB = db.filter(item =>{
+        if(!item[id])
+        return item
+    })
+
+    db = newDB
+    return res.send(newDB)
+})
+
+routes.patch('/:id', (req, res) => {
+    const id = req.params.id
+
+    let newDB = db.filter(item => {
+        if (!item[id])
+            return item
+    })
+
+    db = newDB
+    return res.send(newDB)
+})
+
+
+module.exports = routes
